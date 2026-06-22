c58811167f4742ae62e226893fe7de286c344788
Skelly05 <skelly05@syr.edu>
2026-06-22 15:28:16 -0400

    Removes yellow color from application and adds SU brand

--- pdf_ui/src/components/InformationBlurb.jsx
+++ pdf_ui/src/components/InformationBlurb.jsx
@@ -55,7 +55,7 @@ const InformationBlurb = () => {
         >
           <Box
             sx={{
-              backgroundColor: '#ffc627',
+              backgroundColor: '#000E54',
               padding: { xs: 0.75, sm: 1 },
               borderRadius: 1,
               marginBottom: { xs: 1.5, sm: 2 },
