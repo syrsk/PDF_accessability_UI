4fcc11a0d10473c2148230a4a2964a9a50344898
Skelly05 <skelly05@syr.edu>
2026-09-02 14:38:58 -0400

    edits the left hand side "about this solution" text

--- pdf_ui/src/components/LeftNav.jsx
+++ pdf_ui/src/components/LeftNav.jsx
@@ -90,15 +90,7 @@ const LeftNav = ({ isCollapsed, setIsCollapsed, mobileOpen, setMobileOpen }) => 
               </Typography>
             </Box>
             <Divider sx={{ marginBottom: 2 }} />
-            <Typography variant="body2" gutterBottom>
-              - Each user is limited to <strong>8 PDF document uploads</strong>.
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              - Documents cannot exceed <strong>10 pages</strong>.
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              - Documents must be smaller than <strong>25 MB</strong>.
-            </Typography>
+
             <Typography variant="body2" gutterBottom>
               - Do not upload documents containing <strong>sensitive information</strong>.
             </Typography>
